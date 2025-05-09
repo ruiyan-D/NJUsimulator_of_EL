a71fea0e8d326e44c51aba5e1dd4c561beae974c
Wi1lcOAteR <2207834568@qq.com>
2025-05-09 21:00:30 +0800

    Add files via upload
    新添了跑步，按下左shift的时候可以以两倍速移动

---
+++ Script/Cursor/CursorManager.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CursorManager : MonoBehaviour
+{
+    private Vector3 mouseWorldPos => Camera.main.ScreenToWorldPoint(new Vector3(Input.mousePosition.x, Input.mousePosition.y,0));
+    private bool canClick;
+
+    private void Update(){
+        canClick = ObjectAtMousePosition();
+        if(canClick && Input.GetMouseButtonDown(0)){
+            ClickAction(ObjectAtMousePosition().gameObject);
+        }
+    }
+
+    private void ClickAction(GameObject clickObject){
+        switch(clickObject.tag){
+            case "Teleport":
+                var teleport = clickObject.GetComponent<Teleport>();
+                teleport?.TeleportToScene();
+                break;
+        }
+    }
+
+    private Collider2D ObjectAtMousePosition(){
+        return Physics2D.OverlapPoint(mouseWorldPos);
+    }
+}
